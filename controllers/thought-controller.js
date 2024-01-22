af63febe0df798fa9602dcae658b5666f4547abe
Allex Ortiz <allexortiz@outlook.com>
2024-01-21 23:07:43 -0600

    added thought-routes and thought-controllers code

---
+++ controllers/thought-controller.js
@@ -0,0 +1,107 @@
+// Import the Thought model from the "../models" directory
+const { Thought } = require("../models");
+
+// Import the User model
+const User = require("../models/User");
+
+// Controller object for handling Thought-related operations
+const thoughtController = {
+  // Get all Thoughts
+  getAllThought(req, res) {
+    Thought.find({})
+      .populate({
+        path: "reactions",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // Get one Thought by id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .populate({
+        path: "reactions",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // Create Thought and associate it with a user
+  createThought({ params, body }, res) {
+    Thought.create(body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { _id: params.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.json(err));
+  },
+
+  // Update Thought by id
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought found with this id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  // Delete Thought by id
+  deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.json(err));
+  },
+
+  // Add reaction to a Thought
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $addToSet: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought with this id" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  // Delete reaction from a Thought
+  removeReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.json(err));
+  },
+};
+
+// Export the thoughtController for use in other parts of the application
+module.exports = thoughtController;
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,32 @@
+// Import the Express router module
+const router = require("express").Router();
+
+// Import controller methods from the thought-controller file
+const {
+  getAllThought,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  removeReaction,
+} = require("../../controllers/thought-controller");
+
+// Define routes for the '/api/thoughts' endpoint
+router.route("/")
+  .get(getAllThought)  // GET request to get all thoughts
+  .post(createThought);  // POST request to create a new thought
+
+// Define routes for the '/api/thoughts/:id' endpoint
+router.route("/:id")
+  .get(getThoughtById)  // GET request to get a thought by ID
+  .put(updateThought)   // PUT request to update a thought by ID
+  .delete(deleteThought);  // DELETE request to delete a thought by ID
+
+// Define routes for the '/api/thoughts/:thoughtId/reactions' endpoint
+router.route("/:thoughtId/reactions")
+  .post(createReaction)   // POST request to add a reaction to a thought
+  .delete(removeReaction);  // DELETE request to remove a reaction from a thought
+
+// Export the configured router for use in other parts of the application
+module.exports = router;
